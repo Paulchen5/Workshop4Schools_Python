c96b43eb10eef414be9160a157ace986f0944952
Paulchen <lukas.23022005@gmail.com>
2025-03-09 01:54:41 +0100

    Format `Exercises/03Taschenrechner/ohneTests/main.py`

--- Exercises/03Taschenrechner/ohneTests/main.py
+++ Exercises/03Taschenrechner/ohneTests/main.py
@@ -1,13 +1,14 @@
 def taschenrechner():
-    ''''
+    """'
     fülle diese Funktion:
     1. Lass eine Zahl vom Nutzer bestimmen
     2. Lass eine Operaton(+,-,*,/) von Nutzer bestimmen
     3. Lass die 2. Zahl vom Nutzer bestimmen
     4. Berechne das Ergebnis und gebe es aus
-    '''    
+    """
 
     # Hier kommt dein Code hin
     pass
+
 
 taschenrechner()
